ad179988c0a9eef4defc4d7908c4c27ecfc07c91
David Matheus <davidmatheus75@gmail.com>
2021-04-20 00:40:10 -0300

    Iniciando Projeto

---
+++ app.js
@@ -0,0 +1,35 @@
+//Carregando módulos>>
+
+const express = require('express')
+const handlebars =  require('express-handlebars')
+const bodyParser = require("body-parser")
+const app = express()
+const admin = require("./routes/admin")
+const path = require("path")
+//const mongoose = require("mongoose")
+
+//Configurações>>
+    //BodyParser
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use(bodyParser.json())
+
+    //Handlebars
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars');
+
+    //Mongoose
+
+    //Public
+    app.use(express.static(path.join(__dirname, "public")))
+
+//Rotas>>
+app.use('/admin', admin)
+
+
+
+
+//Outros>>
+const PORT = 8081
+app.listen(PORT, ()=> {
+    console.log("Servidor rodando...")
+})
